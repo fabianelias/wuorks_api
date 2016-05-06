78a933f0e31f1c9708217cf0b7b9f6f01714eec8
fabian <fabian@mini-de-fabian.home>
2016-05-06 01:02:20 -0300

    avances jueves

--- application/models/Job_model.php
+++ application/models/Job_model.php
@@ -129,9 +129,14 @@ Class Job_model extends CI_Model{
         
         $this->db->select('j.key_aviso, j.title, j.description, 
                            j.remuneration, j.created_at, j.delete_at, j.id_user,
-                           j.state, cj.tipo_aviso, cj.genero, cj.horario, cj.zona
+                           j.state, cj.tipo_aviso, cj.genero, cj.horario, cj.zona,
+                           c.company_name, c.company_description,c.address,c.company_category,
+                           c.key_company, c.commune, c.region,
+                           u.username, u.email, u.wuorks_key,u.type_account, u.create_time
                             ');
         $this->db->join('ws_config_job as cj','cj.key_aviso = j.key_aviso','left');
+        $this->db->join('ws_user as u','u.id_user = j.id_user','left');
+        $this->db->join('ws_company as c','c.id_user = u.id_user','left');
         $this->db->where('j.key_aviso',$key_aviso);
         $this->db->where('j.state',0);//0 = activo; 1 = desactivo
         $this->db->order_by('j.id','DESC');
@@ -154,43 +159,84 @@ Class Job_model extends CI_Model{
         //Parametros para selección
        
         //obtener todas las profesiones creadas.
-        $sqlProf = "SELECT
-                    id_profession, 
-                    name_profession, 
-                    job_description,
-                    key_profession, 
-                    id_user
-                    FROM ws_profession
-                    ";
-        $arrProf = $this->db->query($sqlProf);
-        $arrProf = $arrProf->result_array();
         
+        $this->db->select("p.id_profession,
+                           p.name_profession,
+                           p.job_description,
+                           p.key_profession,
+                           p.id_user,
+                           u.username,
+                           u.email,
+                           u.wuorks_key,
+                           u.type_account,
+                           iu.name,
+                           iu.last_name_p,
+                           iu.telephone_number,
+                           iu.cell_phone_number,
+                           iu.address,
+                           iu.commune,
+                           iu.region,
+                           iu.avatar,
+                           iu.gender
+                          ");
+        $this->db->join("ws_profession as p","p.id_user = u.id_user","left");
+        $this->db->join("ws_user_information as iu","iu.id_user = u.id_user","left");
+        $this->db->where("u.state",1);
+        $sqlUser = $this->db->get("ws_user as u");
+        $arrUser = $sqlUser->result_array();
         //iteración 1 sobre los usuarios
         
         $i = 0;
         $matches['users'] = array();
-        while (isset($arrProf[$i]['id_profession'])){
+        //while (isset($arrUser['id_profession'])){
+        //Parametros para selección
+        $k = 0;
+        $j = 0;
+        foreach ($arrUser as $arr){
+           
+           //buscar nombre comuna
+            $this->db->select("nombre");
+            $this->db->where("id",$arr['commune']);
+            $query2  = $this->db->get("comunas");
+            $comuna = $query2->result_array();
+            
+            //echo$infoJob[0]['commune'];exit();
+            //por profesiòn
+            preg_match('/'.strtolower($arr['name_profession']).'/', strtolower($infoJob[0]['title']), $match);
+            
+            //Por genero
+            if($arr['gender'] == $infoJob[0]['genero']){ $gender = "ok";}else{$gender ="";}
             
             
-            //Parametros para selección
-            $k = 0;
-            $j = 0;
-            preg_match('/'.$arrProf[$i]['name_profession'].'/', $infoJob[0]['title'],$match);
+            //Por comuna
+            if($arr['commune'] == $infoJob[0]['commune']){$comuna = "Ok";}else{$comuna ="";}
             
-            if (empty($match)) {
+            
+            if(!empty($match) && !empty($gender) && !empty($comuna)){
+                if($arr['id_profession'] != null){
+                   
+                    $this->db->select_avg('user_rating');
+                    $this->db->where('id_profession',$arr['id_profession']);
+                    $rating = $this->db->get('ws_rating');
+                    
+                    $matches['users'][] = array(
+                        'id'       => $arr['id_profession'],
+                        'username' => $arr['username'],
+                        'name_prof'=> $arr['name_profession'],
+                        'avatar'   => $arr['avatar'],
+                        'rating'   => $rating->row()->user_rating,
+                        'key_prof' => $arr['key_profession'],
+                        'wuorks_key' => $arr['wuorks_key']
+                        );
+                }
+                $k++;
+            }else{
                 $j++;
-               //$matches['users'][] = array('id' => $profesion);
-            } else {
-                $k = 1;
-                $matches['users'][] = array('id' => $arrProf[$i]['id_profession']);
-                
             }
             
             $i++;
         }
-        
-        return $matches;
-        
+       return array_filter($matches); 
     }
 }   
 
